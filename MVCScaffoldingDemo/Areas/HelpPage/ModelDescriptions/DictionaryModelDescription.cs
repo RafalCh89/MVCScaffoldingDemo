354fa792b456b208ffc72674dbf6197b83db1bea
RafalCh89 <rafal.choma@hotmail.com>
2017-04-23 15:29:53 +0100

    Add project files.

---
+++ MVCScaffoldingDemo/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MVCScaffoldingDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MVCScaffoldingDemo/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MVCScaffoldingDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
